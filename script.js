23adb1fc0b2ad2c6215bc0effdcd6523dbb953d7
Chuck Konkol <ckonkol@gmail.com>
2020-10-10 19:48:09 -0500

    Update script.js

--- script.js
+++ script.js
@@ -145,3 +145,14 @@ function clearDiv(elementID) {
                 div.removeChild(div.firstChild); 
             } 
         } 
+function showPosition(position) {
+  x.innerHTML = "Latitude: " + position.coords.latitude + 
+  "<br>Longitude: " + position.coords.longitude;
+}
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else { 
+    x.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}
