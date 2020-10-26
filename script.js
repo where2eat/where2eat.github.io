494a052092d3c854c0090070bf1893ac7b0d2341
Chuck Konkol <ckonkol@gmail.com>
2020-10-26 12:04:29 -0500

    Update script.js

--- script.js
+++ script.js
@@ -211,30 +211,16 @@ try {
         locLink.attr("href", "https://google.com/maps/place/" + restLocation.replace(/\s+/g, "+"));
       locLink.attr("target", "_blank");
       $("#restaurantinfo-div").append(locLink);
-} catch (error) {
-    var restLocation = 'N/A';
-    var locLink = 'N/A';
-    $("#restaurantinfo-div").append(locLink);
-}
-try {
-    $("#restaurantinfo-div").append("<h1>" + data.results[randomInd].name + '</h1>');
-} catch (error) {
-    $("#restaurantinfo-div").append("<h1>" + 'N/A' + '</h1>');
-}
-try {
-         var blnOpen = data.results[randomInd].opening_hours.open_now;
+      $("#restaurantinfo-div").append("<h1>" + data.results[randomInd].name + '</h1>');
+          var blnOpen = data.results[randomInd].opening_hours.open_now;
       if (blnOpen){
  $("#restaurantinfo-div").append("<b>Open Now? </b>" + "Yes<br>");
       }else{
 $("#restaurantinfo-div").append("<b>Open Now? </b>" + "No<br>");
       }
+    $("#restaurantinfo-div").append("<br><img src='https://maps.googleapis.com/maps/api/place/photo?photoreference=" + data.results[randomInd].photos[0].photo_reference + "&sensor=false&maxheight=225&maxwidth=225&key=AIzaSyC2oYu6gWezMlWH0C8ACn2mRl81ISqu4mc'" + "/>");        
 } catch (error) {
-   $("#restaurantinfo-div").append("<b>Open Now? </b>" + "N/A<br>");
-}
-try {
-      $("#restaurantinfo-div").append("<br><img src='https://maps.googleapis.com/maps/api/place/photo?photoreference=" + data.results[randomInd].photos[0].photo_reference + "&sensor=false&maxheight=225&maxwidth=225&key=AIzaSyC2oYu6gWezMlWH0C8ACn2mRl81ISqu4mc'" + "/>");
-} catch (error) {
-     $("#restaurantinfo-div").append("No Photo<br>:");
+    $("#restaurantinfo-div").append("Try Again");
 }
       //"<br><p style='background-color:#64A7FE;color:#FFFFFF'><b>Tap button again for another eatery!</b></p>"
     }
