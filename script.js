4f0102bc4cc93e52199b125dae41976c77dc08d4
Chuck Konkol <ckonkol@gmail.com>
2020-10-28 07:09:47 -0500

    Update script.js

--- script.js
+++ script.js
@@ -139,6 +139,7 @@ function getLocationID(long, lat) {
 $.ajax( {
     url  : 'https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=' + lat + ',' +  long + '&radius=' + randomradius + '&type=restaurant&key=AIzaSyC2oYu6gWezMlWH0C8ACn2mRl81ISqu4mc',
     success : function( data) {
+        pids = '';
         pids = data.results[randomInd].place_id;
         var restLocation = data.results[randomInd].vicinity;
        var city = restLocation.split(",");
@@ -183,7 +184,10 @@ catch(err) {
 });
 }
 function details(){
-$.ajax( {
+    if (pids === ''){
+        $("#restaurantinfo-div").append("Please Try Again!");
+    }else{
+        $.ajax( {
     url  : "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/details/json?place_id=" + pids + "&fields=formatted_phone_number,website&key=AIzaSyC2oYu6gWezMlWH0C8ACn2mRl81ISqu4mc",
     success : function( data) {
         var gphonenum = data.result.formatted_phone_number;
@@ -211,6 +215,8 @@ var queryURL = "https://public.opendatasoft.com/api/records/1.0/search/?dataset=
     document.getElementById('city-input').value = response.records[0].fields.city;
     alert(cityname);
       });
+    }
+
 }
 
 function clearDiv(elementID) { 
